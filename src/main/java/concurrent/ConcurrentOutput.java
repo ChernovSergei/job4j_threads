b5cef95c1e056adcc369bb272175260eb24b3e36
Sergei Chernov <goldengrail@gmail.com>
2024-11-13 17:40:55 +0500

    2. Создание и запуск нити [#1016]

---
+++ src/main/java/concurrent/ConcurrentOutput.java
@@ -0,0 +1,17 @@
+package concurrent;
+
+import java.lang.Thread;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
