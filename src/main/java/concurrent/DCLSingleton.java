377d7ebc985c2891201506355804a8c1367031e3
Sergei Chernov <goldengrail@gmail.com>
2024-11-18 14:44:26 +0500

    2. Модель памяти Java [#267917]

---
+++ src/main/java/concurrent/DCLSingleton.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+public final class DCLSingleton {
+    private static volatile DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private DCLSingleton() {
+    }
+}
