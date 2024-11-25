928351aedccf80df36f41e1a6c7ba0625f19e260
Sergei Chernov <goldengrail@gmail.com>
2024-11-25 19:36:01 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102 #523232]

--- src/main/java/io/ParseFile.java
+++ src/main/java/io/ParseFile.java
@@ -10,12 +10,12 @@ public class ParseFile {
         this.file = file;
     }
 
-    public synchronized String getContent(Predicate<Character> filter, Character limit) {
+    public synchronized String getContent(Predicate<Character> filter) {
         StringBuilder output = new StringBuilder("");
         try (InputStream input = new BufferedInputStream(new FileInputStream(file))) {
             int data;
-            while ((data = input.read()) > 0) {
-                if (filter.test(limit)) {
+            while ((data = input.read()) != 0) {
+                if (filter.test((char) data)) {
                     output.append((char) data);
                 }
             }
