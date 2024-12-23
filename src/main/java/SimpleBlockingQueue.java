10a7c7d003ef516df12bbaf834567c556304a845
Sergei Chernov <goldengrail@gmail.com>
2024-12-23 11:06:02 +0500

    1. Реализовать шаблон Producer Consumer. [#1098 #525899]

--- src/main/java/SimpleBlockingQueue.java
+++ src/main/java/SimpleBlockingQueue.java
@@ -15,7 +15,7 @@ public class SimpleBlockingQueue<T> {
     }
 
     public synchronized void offer(T value) throws InterruptedException {
-                while (queue.size() + 1 >= maxSize) {
+                while (queue.size() > maxSize) {
                     this.wait();
                 }
                 queue.offer(value);
