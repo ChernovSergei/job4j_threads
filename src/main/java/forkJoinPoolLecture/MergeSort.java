8455eee2b42c946e8b93497de7ff7394166f31d1
Sergei <goldengrail@gmail.com>
2025-03-26 15:44:21 +0500

    3. ForkJoinPool [#315067 #537386] lectures

--- src/main/java/forkJoinPoolLecture/MergeSort.java
+++ src/main/java/forkJoinPoolLecture/MergeSort.java
@@ -1,4 +1,48 @@
 package forkJoinPoolLecture;
 
 public class MergeSort {
+
+    public static int[] sort(int[] array) {
+        return sort(array, 0, array.length - 1);
+    }
+
+    private static int[] sort(int[] array, int from, int to) {
+        if (from == to) {
+            return new int[] { array[from] };
+        }
+
+        int middle = (from + to) / 2;
+
+        return merge(
+                sort(array, from, middle),
+                sort(array, middle + 1, to)
+        );
+    }
+
+    public static int[] merge(int[] left, int[] right) {
+        int leftI = 0;
+        int rightI = 0;
+        int resultI = 0;
+        int[] result = new int[left.length + right.length];
+        while (resultI != result.length) {
+            if (leftI == left.length) {
+                result[resultI++] = right[rightI++];
+            } else if (rightI == right.length) {
+                result[resultI++] = left[leftI++];
+            } else if (left[leftI] <= right[rightI]) {
+                result[resultI++] = left[leftI++];
+            } else {
+                result[resultI++] = right[rightI++];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 44, 22, 1354, 4};
+        int[] sortedArray = ParallelMergeSort.sort(array);
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println(sortedArray[i]);
+        }
+    }
 }
