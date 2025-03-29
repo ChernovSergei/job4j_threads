71fb8686a2127093d1ccce90cb5a9c467edda38f
Sergei <goldengrail@gmail.com>
2025-03-29 23:10:24 +0500

    3. ForkJoinPool [#315067 #537386]

--- src/main/java/forkjoin/ParallelSearch.java
+++ src/main/java/forkjoin/ParallelSearch.java
@@ -3,13 +3,13 @@ package forkjoin;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelSearch extends RecursiveTask<Integer> {
-    private final Object[] array;
+public class ParallelSearch<T> extends RecursiveTask<Integer> {
+    private final T[] array;
     private final int from;
     private final int to;
-    private final Object wantedObject;
+    private final T wantedObject;
 
-    public ParallelSearch(Object[] array, int from, int to, Object wantedObject) {
+    public ParallelSearch(T[] array, int from, int to, T wantedObject) {
         this.array = array;
         this.from = from;
         this.to = to;
@@ -18,36 +18,30 @@ public class ParallelSearch extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-        if (from == to) {
-            return array[from].equals(wantedObject) ? from : null;
+        if (to - from < 10) {
+            return indexLinearSearch(from, to);
         }
         int middle = (from + to) / 2;
-        ParallelSearch leftSort = new ParallelSearch(array, from, middle, wantedObject);
-        ParallelSearch rightSort = new ParallelSearch(array, middle + 1, to, wantedObject);
+        ParallelSearch<T> leftSort = new ParallelSearch<>(array, from, middle, wantedObject);
+        ParallelSearch<T> rightSort = new ParallelSearch<>(array, middle + 1, to, wantedObject);
         leftSort.fork();
         rightSort.fork();
         Integer leftResult = leftSort.join();
         Integer rightResult = rightSort.join();
-        if (leftResult != null) {
-            return leftResult;
-        }
-        if (rightResult != null) {
-            return rightResult;
-        }
-        return null;
+        return (leftResult == -1 && rightResult == -1) ? null : Math.max(leftResult, rightResult);
     }
 
-    public static Integer getIndex(Object[] array, Object element) {
-        Integer result = null;
-        if (array.length < 10) {
-            for (int i = 0; i < array.length; i++) {
-                if (array[i].equals(element)) {
-                    result = i;
-                }
+    public static <T> Integer getIndex(T[] array, T wantedObject) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return (Integer) forkJoinPool.invoke(new ParallelSearch(array, 0, array.length - 1, wantedObject));
+    }
+
+    private int indexLinearSearch(int from, int to) {
+        int result = -1;
+        for (int i = from; i < to; i++) {
+            if (array[i].equals(wantedObject)) {
+                result = i;
             }
-        } else {
-            ForkJoinPool forkJoinPool = new ForkJoinPool();
-            result = forkJoinPool.invoke(new ParallelSearch(array, 0, array.length - 1, element));
         }
         return result;
     }
